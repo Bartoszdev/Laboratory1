644a2f90909ee27992843a35ded1f4a6940b4e64
Bartek <bartosz@e-mo.pl>
2018-01-31 21:59:38 +0100

    Laboratorium (początek)

---
+++ Laboratory1/Greyhound.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratory1
+{
+    public class Greyhound
+    {
+        public int StartingPosition;
+        public int RacetrackLenght;
+        public PictureBox MyPictureBox = null;
+        public int Location = 0;
+        public Random MyRandom;
+    
+
+    public bool Run()
+    {
+           string )]
+    }
+}
